6cc0f7f9d22bd693535ce57fb88323c4a26abc03
Dima.A <dima.a@systeamd.com>
2019-05-06 09:39:16 +0300

    disable search action

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -3,16 +3,16 @@ import PropTypes from 'prop-types';
 import '../assets/css/search.css';
 
 function Search(props) {
-	function runSearch() {
+	function runAction(action, payload = null) {
 		if (props.value !== '') {
-			props.onSearch(props.value)
+			payload ? action(payload) : action()
 		}
 	}
 	return (
 		<div className="wrapp-search">
 			<input type="text" onChange={(event) => props.onTyping(event.target.value)} value={props.value} placeholder="search..." />
-			<button onClick={runSearch} className="btn-search btn-search--primary">Search</button>
-			<button onClick={props.onReset} className="btn-search btn-search--reset">Reset</button>
+			<button onClick={() => runAction(props.onSearch, props.value)} className="btn-search btn-search--primary">Search</button>
+			<button onClick={() => runAction(props.onReset)} className="btn-search btn-search--reset">Reset</button>
 		</div>
 	);
 };
